3c2fd879848ae6c28b94813e301e19e8808f5191
twinh <twinhuang@qq.com>
2021-05-30 18:27:11 +0800

    feat(taro): 增加 `ret`、`suc` 和 `err` 接口

--- index.js
+++ index.js
@@ -20,6 +20,46 @@ $.confirm = (message, fn) => {
   });
 };
 
+$.ret = (ret, duration = 3000, callback) => {
+  const result = Taro.showToast({
+    title: ret.message,
+    icon: 'none',
+    duration,
+    success: callback,
+  });
+
+  let suc;
+  result.suc = fn => {
+    suc = fn;
+    return result;
+  };
+
+  let err;
+  result.err = fn => {
+    err = fn;
+    return result;
+  };
+
+  result.then(() => {
+    if (ret.code === 0 && suc) {
+      suc();
+    }
+    if (ret.code !== 0 && err) {
+      err();
+    }
+  });
+
+  return result;
+};
+
+$.suc = (message, duration, callback) => {
+  return $.ret({code: 0, message}, duration, callback);
+};
+
+$.err = (message, duration, callback) => {
+  return $.ret({code: 1, message}, duration, callback);
+};
+
 $.loading = (options = 'show') => {
   switch (options) {
     case 'show':
