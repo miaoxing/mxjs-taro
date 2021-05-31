6836804e9418a06a85104916451506d8237bb677
twinh <twinhuang@qq.com>
2021-05-31 10:35:13 +0800

    test(taro): 增加 `url` 接口测试

---
+++ __tests__/index.js
@@ -0,0 +1,21 @@
+import '../';
+import $ from 'miaoxing';
+
+describe('taro', () => {
+  test('url: add /pages', () => {
+    expect($.url('tests/edit')).toBe('/pages/tests/edit');
+    expect($.url('tests/edit', {id: 1})).toBe('/pages/tests/edit?id=1');
+    expect($.url('tests/edit?id=1', {id: 2})).toBe('/pages/tests/edit?id=1&id=2');
+  });
+
+  test('url: add /index', () => {
+    expect($.url('tests')).toBe('/pages/tests/index');
+    expect($.url('tests', {a: 'b'})).toBe('/pages/tests/index?a=b');
+    expect($.url('tests?a=b', {a: 'c'})).toBe('/pages/tests/index?a=b&a=c');
+  });
+
+  test('url: ignore absolute path', () => {
+    expect($.url('/pages/test')).toBe('/pages/test');
+    expect($.url('/pages/test/index')).toBe('/pages/test/index');
+  });
+});
