28054e9bdc3a636440f13c8146761fdaf5131f09
twinh <twinhuang@qq.com>
2021-09-16 10:19:56 +0800

    test(taro): mock h5 不支持的 `showNavigationBarLoading` 接口

--- __tests__/http.js
+++ __tests__/http.js
@@ -7,12 +7,15 @@ Taro.getStorageSync = jest.fn();
 Taro.showToast = jest.fn().mockImplementation(() => {
   return {};
 });
+// h5 不支持该接口，需要 mock
+Taro.showNavigationBarLoading = jest.fn();
 
 describe('http', () => {
   test('patch', async () => {
     const promise = createPromise();
     Taro.request = jest.fn().mockImplementationOnce(() => promise.resolve({
       statusCode: 200,
+      data: {},
     }));
 
     await $.http({
